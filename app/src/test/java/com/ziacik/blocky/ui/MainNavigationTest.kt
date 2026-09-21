edeeef356364ef02ae69e9c12155ff2ab7d569fc
František Žiačik <ziacik@gmail.com>
2026-09-21 23:05:39 +0200

    test: define receipt detail and Wolt item pricing

---
+++ app/src/test/java/com/ziacik/blocky/ui/MainNavigationTest.kt
@@ -0,0 +1,30 @@
+package com.ziacik.blocky.ui
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MainNavigationTest {
+	@Test
+	fun clickingReceiptRequestsItsDetail() {
+		assertEquals(
+			MainScreen.ReceiptDetail("wolt:123"),
+			MainNavigation.reduce(MainScreen.Home, MainIntent.OpenReceipt("wolt:123")),
+		)
+	}
+
+	@Test
+	fun clickingAllItemsOpensGlobalItemList() {
+		assertEquals(
+			MainScreen.AllItems,
+			MainNavigation.reduce(MainScreen.Home, MainIntent.OpenAllItems),
+		)
+	}
+
+	@Test
+	fun backReturnsHome() {
+		assertEquals(
+			MainScreen.Home,
+			MainNavigation.reduce(MainScreen.ReceiptDetail("wolt:123"), MainIntent.Back),
+		)
+	}
+}
