cc3c68dd0788bb627be3d24cd2059ec961899b8e
František Žiačik <ziacik@gmail.com>
2026-09-21 22:48:05 +0200

    test: match current Wolt web auth headers

--- app/src/test/java/com/ziacik/blocky/data/wolt/WoltRequestHeadersTest.kt
+++ app/src/test/java/com/ziacik/blocky/data/wolt/WoltRequestHeadersTest.kt
@@ -1,28 +1,21 @@
 package com.ziacik.blocky.data.wolt
 
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Test
 
 class WoltRequestHeadersTest {
 	@Test
-	fun deviceHeadersMatchTelemetryCookies() {
-		val headers = WoltRequestHeaders.fromCookies(
-			"foo=bar; telemetryDeviceId=device-123; telemetrySessionId=session-456; __wtoken=token"
+	fun buildsCurrentWoltWebIdentityHeaders() {
+		val headers = WoltRequestHeaders.create(
+			webClientId = "client-123",
+			accessToken = "abc.def.ghi",
 		)
 
-		assertEquals("device-123", headers["x-wolt-web-clientid"])
-		assertEquals("session-456", headers["w-wolt-session-id"])
+		assertEquals("client-123", headers["x-wolt-web-clientid"])
+		assertEquals("no-analytics-consent", headers["w-wolt-session-id"])
+		assertEquals("Bearer abc.def.ghi", headers["Authorization"])
 		assertEquals("Web", headers["platform"])
-		assertEquals("https://wolt.com", headers["Origin"])
-		assertEquals("https://wolt.com/", headers["Referer"])
-	}
-
-	@Test
-	fun doesNotInventDeviceIdsWhenCookiesDoNotContainThem() {
-		val headers = WoltRequestHeaders.fromCookies("__wtoken=token")
-
-		assertFalse(headers.containsKey("x-wolt-web-clientid"))
-		assertFalse(headers.containsKey("w-wolt-session-id"))
+		assertEquals("1.16.79", headers["client-version"])
+		assertEquals("1.16.79", headers["clientversionnumber"])
 	}
 }
